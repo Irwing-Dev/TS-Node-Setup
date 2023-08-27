a74141cfcec707a64a604d55f312f61c619a3e23
Irwing <irwing.devbr@gmail.com>
2023-08-27 19:53:37 -0300

    chore: hidden dist/

--- dist/index.spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require(".");
-it("MyName", () => {
-    const person = new _1.default();
-    expect(person.myName()).toBe("Irwing-Dev");
-});
